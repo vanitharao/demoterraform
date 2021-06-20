0d9a5a28def557afeedce8f27b0a661f92b804e8
ambati manoj <ambatimanoj01@gmail.com>
2021-06-20 20:50:12 +0530

    changes made

--- jenkinsinstall.tf
+++ jenkinsinstall.tf
@@ -4,10 +4,10 @@
 # Azure Provider source and version being used
 terraform {
   required_providers {
-    azurerm = {
+    
       source  = "hashicorp/azurerm"
       version = "=2.46.0"
-    }
+    
   }
 }
 
