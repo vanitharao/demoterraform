bc29b04aca23591c2236dfa948bc992659d9bc58
ambati manoj <ambatimanoj01@gmail.com>
2021-06-20 17:38:07 +0530

    changes made

--- jenkinsinstall.tf
+++ jenkinsinstall.tf
@@ -4,6 +4,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "1.0.0"
     }
+    backend "azurerm" {
+    resource_group_name  = "rg-terraformstate"
+    storage_account_name = "terrastatestorage21345"
+    container_name       = "terraformdemo"
+    key                  = "dev.terraform.tfstate"
+  }
   }
 }
 provider "azurerm" {
