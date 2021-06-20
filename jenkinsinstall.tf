9826271736b43fc26ab9f7aa45c14d793bcd458a
ambati manoj <ambatimanoj01@gmail.com>
2021-06-20 20:39:18 +0530

    code changed

--- jenkinsinstall.tf
+++ jenkinsinstall.tf
@@ -1,17 +1,25 @@
+variable "client_secret" {
+}
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.64.0"
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  # Configuration options
-alias = "jenkinsvm"
-subscription_id = var.subscription_id
-tenant_id       = var.tenant_id  
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
  resource "azurerm_resource_group" "test" {
