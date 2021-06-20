5f5094b887a7d3a0c0f0fe6ce28f2db0964bec25
ambati manoj <ambatimanoj01@gmail.com>
2021-06-20 20:19:15 +0530

    changes made

--- jenkinsinstall.tf
+++ jenkinsinstall.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=1.0.0"
+      source = "hashicorp/azurerm"
+      version = "2.64.0"
     }
   }
 }
-    
+
 provider "azurerm" {
-features {}
+  # Configuration options
 alias = "jenkinsvm"
 subscription_id = var.subscription_id
 tenant_id       = var.tenant_id  
