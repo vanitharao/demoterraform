3e6dc7a6a4f9a286c3aa527ffd6227974205bbb5
ambati manoj <ambatimanoj01@gmail.com>
2021-06-21 21:11:20 +0530

    nothing changed

--- jenkinsinstall.tf
+++ jenkinsinstall.tf
@@ -24,7 +24,7 @@ resource "azurerm_virtual_network" "test" {
   resource_group_name = azurerm_resource_group.test.name
 }
 
-resource "azurerm_subnet" "test" {
+resource "azurerm_subnet" "test" {   
   name                 = var.subnet
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
@@ -81,7 +81,7 @@ resource "azurerm_virtual_machine" "test" {
     caching       = "ReadWrite"
     create_option = "FromImage"
   }
- 
+  
   os_profile {
     computer_name  = "hostname"
     admin_username = "testadmin"
